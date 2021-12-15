821cd14591a9f72cce81fbe4f8fc7bbea8db1b08
David Morgan <david.morgan@europe.com>
2021-12-15 20:52:41 +0000

    TileList implementation

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import { Tile } from '../tile/Tile';
 
-export const TileList = () => {
+export const TileList = ({
+  objectArray
+}) => {
   return (
     <div>
-      
+      {
+        objectArray.map(o => <Tile object={o} />)
+      }
     </div>
   );
+};
+
+TileList.propTypes = {
+  objectArray: PropTypes.array
 };
