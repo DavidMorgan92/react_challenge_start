1063e2469f0b632b3e40437fc942f242d579f1bf
David Morgan <david.morgan@europe.com>
2021-12-15 22:54:21 +0000

    ContactPicker implementation

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,21 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-export const ContactPicker = () => {
+export const ContactPicker = ({
+  contacts,
+  onChange
+}) => {
   return (
-    ContactPicker
+    <select onChange={onChange}>
+      <option value={-1}>-- No contact --</option>
+      {
+        contacts.map((c, index) => <option key={index} value={index}>{c.name}</option>)
+      }
+    </select>
   );
+};
+
+ContactPicker.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired
 };
