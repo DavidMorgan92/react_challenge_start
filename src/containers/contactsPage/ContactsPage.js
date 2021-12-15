486dd75c816a5daeb535540d6e9539f3aac2d62f
David Morgan <david.morgan@europe.com>
2021-12-15 20:31:05 +0000

    Handle ContactForm submit and prevent duplicates

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -12,6 +12,7 @@ export const ContactsPage = ({
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
+  const [duplicateExists, setDuplicateExists] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,17 +20,29 @@ export const ContactsPage = ({
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if (!duplicateExists) {
+      addContact({name, phone, email});
+      setName('');
+      setPhone('');
+      setEmail('');
+    }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
+  useEffect(() => {
+    if (contacts.some(c => c.name === name)) {
+      setDuplicateExists(true);
+    }
+  }, [contacts, name]);
 
   return (
     <div>
       <section>
         <h2>Add Contact</h2>
+        {duplicateExists && <p>A contact with this name already exists</p>}
         <ContactForm
           name={name}
           setName={setName}
