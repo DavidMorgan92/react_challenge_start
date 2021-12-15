5c3861d1fa5a1b707861393ce242558b3abdad08
David Morgan <david.morgan@europe.com>
2021-12-15 21:04:33 +0000

    Tile implementation

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,24 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-export const Tile = () => {
+export const Tile = ({
+  object
+}) => {
   return (
     <div className="tile-container">
-      
+      {
+        object.values.map((value, index) => {
+          return (
+            <p className={index === 0 ? 'tile-title' : 'tile'}>
+              {value}
+            </p>
+          );
+        })
+      }
     </div>
   );
+};
+
+Tile.propTypes = {
+  object: PropTypes.object.isRequired
 };
