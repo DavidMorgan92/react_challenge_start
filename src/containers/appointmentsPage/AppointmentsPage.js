6188fd4bf7e133a04bb3343237e6ab427aab1900
David Morgan <david.morgan@europe.com>
2021-12-15 21:15:36 +0000

    AppointmentsPage implementation

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
+import { AppointmentForm } from '../../components/appointmentForm/AppointmentForm';
+import { TileList } from '../../components/tileList/TileList';
 
 export const AppointmentsPage = ({
   appointments,
@@ -10,23 +12,65 @@ export const AppointmentsPage = ({
   Define state variables for 
   appointment info
   */
+  const [title, setTitle] = useState('');
+  const [contact, setContact] = useState({});
+  const [date, setDate] = useState(null);
+  const [time, setTime] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
+    addAppointment({
+      title,
+      contact,
+      date,
+      time
+    });
+    setTitle('');
+    setContact({});
+    setDate(null);
+    setTime(null);
+  };
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleContactChange = (e) => {
+    setContact(e.target.value);
+  };
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleTimeChange = (e) => {
+    setTime(e.target.value);
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          contacts={contacts}
+          title={title}
+          setTitle={handleTitleChange}
+          contact={contact}
+          setContact={handleContactChange}
+          date={date}
+          setDate={handleDateChange}
+          time={time}
+          setTime={handleTimeChange}
+          onSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList objectArray={appointments} />
       </section>
     </div>
   );
